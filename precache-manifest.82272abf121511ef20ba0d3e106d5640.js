4098ebb9c9878ba3b23a2487d5c4c8ac67061657
baspavlou <baspavlou@hotmail.com>
2020-03-26 00:08:37 +0200

    Updates

---
+++ precache-manifest.82272abf121511ef20ba0d3e106d5640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632e0ea2567a601a9107cb0254c063aa",
+    "url": "/simple-expenses-app-with-react-hooks/index.html"
+  },
+  {
+    "revision": "1435310ae9e1c03a726a",
+    "url": "/simple-expenses-app-with-react-hooks/static/css/main.788d0cbb.chunk.css"
+  },
+  {
+    "revision": "fc52af795522972e4960",
+    "url": "/simple-expenses-app-with-react-hooks/static/js/2.a6223373.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-expenses-app-with-react-hooks/static/js/2.a6223373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1435310ae9e1c03a726a",
+    "url": "/simple-expenses-app-with-react-hooks/static/js/main.c9e25e5e.chunk.js"
+  },
+  {
+    "revision": "84ae79c3b688c2d409d7",
+    "url": "/simple-expenses-app-with-react-hooks/static/js/runtime-main.7859e675.js"
+  }
+]);
