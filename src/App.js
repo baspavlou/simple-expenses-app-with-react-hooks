e233d0a1a0529b5be9a9a54361f96811580ff7ea
baspavlou <baspavlou@hotmail.com>
2020-03-25 15:23:01 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
+import uuid from "uuid/v4";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialExpenses = [
+    { id: uuid(), charge: "rent", amount: 1600 },
+    { id: uuid(), charge: "car payment", amount: 500 },
+    { id: uuid(), charge: "credit card", amount: 250 }
+];
+const App = () => {
+    const [expenses, setExpenses] = useState(initialExpenses);
+    const [charge, setCharge] = useState("");
+    const [amount, setAmount] = useState("");
+
+    return (
+        <>
+            <Alert />
+            <h1>Budget calculator</h1>
+            <main className="App">
+                <ExpenseForm />
+                <ExpenseList expenses={expenses} />
+            </main>
+            <h1>
+                Total Spending :
+                <span className="total">
+                    ${expenses.reduce((total, curr) => total + curr.amount, 0)}
+                </span>
+            </h1>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+
+const ExpenseList = ({ expenses }) => {
+    return (
+        <>
+            <ul className="list">
+                {expenses.map(expense => (
+                    <ExpenseItem key={expense.id} expense={expense} />
+                ))}
+            </ul>
+            {expenses.length > 0 && (
+                <button className="btn">
+                    Clear Expenses <MdDelete className="btn-icon" />
+                </button>
+            )}
+        </>
+    );
+};
+
+export default ExpenseList;
